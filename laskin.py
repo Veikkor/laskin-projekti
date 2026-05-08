06cd3ac419c20433b012f4d494ba0110eaee944c
veikka-viljami.lampi <veikka-viljami.lampi@edu.keuda.fi>
2026-05-08 12:04:45 +0300

    Lisätty kertolasku-funktio

--- laskin.py
+++ laskin.py
@@ -3,15 +3,14 @@ print("Laskin v1.0")
 def yhteenlasku(a, b):
     return a + b
  
-print("Laskin v1.0")
-print("5 + 3 =", yhteenlasku(5, 3))
-def yhteenlasku(a, b):
-    return a + b
- 
 def vähennyslasku(a, b):
     return a - b
+
+def vähennyslasku(a, b):
+    return a * b
  
 print("Laskin v1.0")
 print("5 + 3 =", yhteenlasku(5, 3))
 print("10 - 4 =", vähennyslasku(10, 4))
+print("10 * 4 =", kertolasku(10, 4))
 
